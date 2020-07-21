f5fb8f2432bb5a43378a3687db352a461a2b6ad5
TomF46 <t-d-f@live.co.uk>
2020-07-21 15:33:23 +0100

    Add fonts

--- src/App.vue
+++ src/App.vue
@@ -35,7 +35,7 @@ html{
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
