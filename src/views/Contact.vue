ad5386895543c738f160d9c6fc3ec643dbd3042d
TomF46 <t-d-f@live.co.uk>
2021-07-19 11:29:23 +0100

    Update some copy

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -10,10 +10,11 @@
         <div class="row">
         <div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3  card">
           <p>
-            Im Tom a Web application developer with multi year experience in developing applications using .NET and cutting edge modern front end technologies and frameworks (e.g. VueJs, ES6 Javascript, HTML5, CSS3). Keen interest in the implementation of the latest browser/ front end features to create the best possible interfaces and experience for users as the web evolves. <br><br>
-            I have worked with and take an intrest in the use of PWA and Hybrid web apps to produce native like mobile experiences for users and have worked on apps that have been released on both the Apple App store and google play store and have a good understanding of the processes required to release an application to the public and following the app store processes <br><br>
-            I also have knowledge in a range of AWS services and how to use these to host websites, store files and data and run web services to support applications. Services i have heavily used include, S3, Amplify, Lambda , and Route53 and a number of these have been used in the projects on my projects page. <br><br>
-            Outside of development i play as a forward for a field hockey team in my home town, play the bass guitar and badly play the electric guitar.
+            I'm Tom a Web application developer with multiyear experience in developing applications using .NET and cutting edge modern front end technologies and frameworks (e.g. VueJS, ES6 JavaScript, HTML5, CSS3). Keen interest in the implementation of the latest browser/ front end features to create the best possible interfaces and experience for users as the web evolves. <br><br>
+            I have worked with and take an interest in the use of PWA and Hybrid web apps to produce native like mobile experiences for users and have worked on apps that have been released on both the Apple App store and Google Play Store and have a good understanding of the processes required to release an application to the public and following the app store processes <br><br>
+            I also have knowledge in a range of AWS services and how to use these to host websites, store files and data and run web services to support applications. Services I have heavily used include, S3, Amplify, Lambda, and Route53 and a number of these have been used in the projects on my projects page. <br><br>
+            I also have experience with Laravel, examples of which can be found in the projects section of this site. <br><br>
+            Outside of work I'm a keen runner, hockey player and occasionally play bass guitar.
           </p>
         </div>
         </div>
